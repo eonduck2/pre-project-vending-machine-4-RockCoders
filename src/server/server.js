fe05c4ec67900736dcb8c32c929d2f743d151501
eonduck2 <whdtnxd@gmail.com>
2024-06-21 10:45:43 +0900

    :bulb: 이종수: 주석 추가

--- src/server/server.js
+++ src/server/server.js
@@ -3,8 +3,10 @@ import path from "node:path";
 
 const app = express();
 
+// * ESM 방식의 __dirname 재정의
 const __dirname = path.resolve();
 
+// * 미들웨어로 등록하기 위한 경로 설정
 const publicPath = path.join(__dirname, "public");
 const srcPath = path.join(__dirname, "src");
 
@@ -12,6 +14,7 @@ const srcPath = path.join(__dirname, "src");
 app.set(`PORT`, process.env.PORT ?? 8080);
 const PORT = app.get(`PORT`);
 
+// * 미들웨어 등록
 app.use("/public", express.static(publicPath));
 app.use("/src", express.static(srcPath));
 
