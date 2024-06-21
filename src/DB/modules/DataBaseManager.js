5c01bb0e2c90e5db022f3961c8fa538ee4119169
eonduck2 <whdtnxd@gmail.com>
2024-06-21 15:38:38 +0900

    :sparkles: 이종수: db 연결 생성자 함수 구현

--- src/DB/modules/DataBaseManager.js
+++ src/DB/modules/DataBaseManager.js
@@ -1,5 +1,19 @@
+import sqlite3 from "sqlite3";
+
+// * sqlite3 객체와 같은 기능을 하지만, 디버깅에 더 유리한 옵션을 포함한 verbose mode로 선언
+const sqlite3VM = sqlite3.verbose();
+
 export default class DataBaseManager {
-  constructor() {}
+  db;
+  constructor(path) {
+    this.db = new sqlite3.Database(path, (err) => {
+      if (err) {
+        console.error("DB 연결 실패: ", err);
+      } else {
+        console.log("DB 연결 성공");
+      }
+    });
+  }
 
   static tableCreator() {}
 
