7fcf7b975d9ae157b87861c571e04cac7d6da001
yuxincxoi <yjyj0774@gmail.com>
2024-06-21 12:47:00 +0900

    :wrench: 최유진 : 로컬스토리지에 잔액 저장

---
+++ src/controllers/purchase.js
@@ -0,0 +1,11 @@
+/**
+ * @yuxincxoi 24.06.20
+ * * 구매하기 버튼 click 시에 실행될 함수입니다.
+ */
+function purchase() {
+  // todo 구매 서버요청하는 부분
+
+  // * 로컬스토리지에 잔액 저장
+  localStorage.setItem("balance", balance);
+  console.log("구매하기 click");
+}
