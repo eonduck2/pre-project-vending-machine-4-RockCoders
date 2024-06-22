97b337e12a1aa1c3161f78013bd548b7acafae84
eonduck2 <whdtnxd@gmail.com>
2024-06-23 03:33:59 +0900

    :bulb: 이종수: 주석 수정

--- src/DB/modules/DataBaseManager.js
+++ src/DB/modules/DataBaseManager.js
@@ -536,26 +536,27 @@ export default class DataBaseManager {
   }
 
   /**
-   * 특정 테이블의 컬럼 순서 변경 및 데이터 복사
-   * @param {string} tableName 변경할 테이블 이름
-   * @param {Object} columns 컬럼 이름과 타입을 정의한 객체
+   * @eonduck2 24.06.23
+   * * 특정 테이블의 컬럼 순서 변경 및 데이터 복사
+   * @param { string } tableName 변경할 테이블 이름
+   * @param { object } columns 컬럼 이름과 타입을 정의한 객체
    */
   reorderColumns(tableName, columns) {
-    // 1. 새로운 테이블 이름을 생성
+    // * 1. 새로운 테이블 이름을 생성
     const newTableName = `${tableName}_temp`;
 
-    // 2. 새로운 테이블의 컬럼 정의 SQL 생성
+    // * 2. 새로운 테이블의 컬럼 정의 SQL 생성
     const columnsDefinition = Object.entries(columns)
       .map(([name, type]) => `${name} ${type}`)
       .join(", ");
 
-    // 3. 새로운 테이블 생성
+    // * 3. 새로운 테이블 생성
     const createTableSql = `CREATE TABLE ${newTableName} (${columnsDefinition})`;
     this.db.run(createTableSql, (err) => {
       if (err) {
         throw new Error(`새로운 테이블 생성 오류`);
       } else {
-        // 4. 데이터 복사 및 순서 맞춤
+        // * 4. 데이터 복사 및 순서 맞춤
         const copyDataSql = `
           INSERT INTO ${newTableName} (${Object.keys(columns).join(", ")})
           SELECT ${Object.keys(columns).join(", ")} FROM ${tableName}`;
@@ -563,13 +564,13 @@ export default class DataBaseManager {
           if (err) {
             throw new Error(`데이터 복사 오류`);
           } else {
-            // 5. 기존 테이블 삭제
+            // * 5. 기존 테이블 삭제
             const dropTableSql = `DROP TABLE ${tableName}`;
             this.db.run(dropTableSql, (err) => {
               if (err) {
                 throw new Error(`기존 테이블 삭제 오류`);
               } else {
-                // 6. 새로운 테이블 이름을 기존 테이블 이름으로 변경
+                // * 6. 새로운 테이블 이름을 기존 테이블 이름으로 변경
                 const renameTableSql = `ALTER TABLE ${newTableName} RENAME TO ${tableName}`;
                 this.db.run(renameTableSql, (err) => {
                   if (err) {
