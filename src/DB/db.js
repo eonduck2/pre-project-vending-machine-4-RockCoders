3794c0ca76b3179a03cc32007c532c491a2958d8
eonduck2 <whdtnxd@gmail.com>
2024-06-23 14:00:07 +0900

    :fire: 이종수: db.js 코드 삭제

--- src/DB/db.js
+++
@@ -1,6 +0,0 @@
-import sqlite3 from "sqlite3";
-
-// * sqlite3 객체와 같은 기능을 하지만, 디버깅에 더 유리한 옵션을 포함한 verbose mode로 선언
-const sqlite3VM = sqlite3.verbose();
-import DBmanager from "./modules/DBMANAGER.js";
-const test = new DBmanager(`123.db`);
