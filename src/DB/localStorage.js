d42e4f0ad05e7b2f8e68961f20ebb78dc22ba801
yuxincxoi <yjyj0774@gmail.com>
2024-06-24 17:07:21 +0900

    localStorage 모델 구현

--- src/DB/localStorage.js
+++ src/DB/localStorage.js
@@ -1,13 +1,68 @@
-/**
- * @yuxincxoi 24.06.20
- * * 구매하기 버튼 click 시에 실행될 함수입니다.
- */
-export function purchase() {
-  // todo 구매 서버요청하는 부분
+import { LocalStorage } from 'node-localstorage';
+const localStorage = new LocalStorage();
 
-  let balance;
+class LocalStorageModel {
+  constructor() {
+    this.validKeys = ["totalPrice", "amount", "balance"];
+  }
 
-  // * 로컬스토리지에 잔액 저장
-  localStorage.setItem("balance", balance);
-  console.log("구매하기 click");
+  // * 유효한 키인지 확인
+  isValidKey(key) {
+    return this.validKeys.includes(key);
+  }
+
+  /**
+   * @yuxincxoi 24.06.24
+   * * 로컬스토리지에 데이터 저장
+   * @param {string} key totalPrice||amount||balance
+   * @param {number} value 총액||입금액||잔액
+   */
+  setItem(key, value) {
+    if (this.isValidKey(key)) {
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      console.error(`Invalid key: ${key}.`);
+    }
+  }
+
+  /**
+   * @yuxincxoi 24.06.24
+   * * 로컬스토리지에서 데이터 가져오기
+   * @param {string} key totalPrice||amount||balance
+   */
+  getItem(key) {
+    if (this.isValidKey(key)) {
+      const value = localStorage.getItem(key);
+      return value ? JSON.parse(value) : null;
+    } else {
+      console.error(`Invalid key: ${key}.`);
+      return null;
+    }
+  }
+
+  /**
+   * @yuxincxoi 24.06.24
+   * * 로컬스토리지에서 데이터 삭제
+   * @param {string} key totalPrice||amount||balance
+   */
+  removeItem(key) {
+    if (this.isValidKey(key)) {
+      localStorage.removeItem(key);
+    } else {
+      console.error(`Invalid key: ${key}.`);
+    }
+  }
+
+  /**
+   * @yuxincxoi 24.06.24
+   * * 로컬스토리지의 모든 데이터 삭제
+   */
+  clear() {
+    localStorage.clear();
+  }
 }
+
+export default LocalStorageModel;
+
+// const localData = new LocalStorageModel();
+// localData.setItem('totalPrice', 3000);
