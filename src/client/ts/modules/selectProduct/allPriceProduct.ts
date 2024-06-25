f9f1b2d94389638cb33233c93704767dfc897cde
jojayeon <jojayeon6152@gmail.com>
2024-06-25 14:34:54 +0900

    :sparkles: 조자연 : 제품 총액 계산 함수 생성

--- src/client/ts/modules/selectProduct/allPriceProduct.ts
+++ src/client/ts/modules/selectProduct/allPriceProduct.ts
@@ -1,1 +1,9 @@
 // * 제품 총액 계산 함수
+export function calculateTotalPrice(selectedProducts: any[]): number {
+  // 선택한 제품들을 기반으로 총액 계산 로직 구현
+  let totalPrice = 0;
+  selectedProducts.forEach(product => {
+    totalPrice += product.price;
+  });
+  return totalPrice;
+}
