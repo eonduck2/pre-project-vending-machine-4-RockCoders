500613bea3513528bd03caccf5462396abad04f5
jojayeon <jojayeon6152@gmail.com>
2024-06-25 16:12:31 +0900

    :sparkles: 조자연 : 선택한 제품 출력하는 함수

--- src/client/ts/modules/selectProduct/selectProductOutput.ts
+++ src/client/ts/modules/selectProduct/selectProductOutput.ts
@@ -1,1 +1,14 @@
 // * 선택한 제품 출력하는 함수
+
+
+export function displaySelectedProducts(selectedProducts: { name: string, price: number }[]) {
+  const productListElement = document.getElementById('total-price');
+  if (productListElement) {
+    productListElement.innerHTML = ''; // 기존 목록 삭제
+    selectedProducts.forEach(product => {
+      const li = document.createElement('li');
+      li.textContent = `${product.name} : ${product.price}원`;
+      productListElement.appendChild(li);
+    });
+  }
+}
