9ba67e9a6802cafbb6294bad882ad27fb8d04e3e
eonduck2 <whdtnxd@gmail.com>
2024-06-25 16:28:39 +0900

    :bulb: 이종수: 주석 추가

---
+++ src/DB/db.js
@@ -0,0 +1,6 @@
+import DataManipulator from "./modules/manipulation/DataManipulator.js";
+import TableCreator from "./modules/table/TableCreator.js";
+
+const test = new DataManipulator(`123.db`);
+
+test.
