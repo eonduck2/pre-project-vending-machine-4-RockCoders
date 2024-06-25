d5d4ec1210fcd6d4ee3ea43ba1078214e2a8a58d
jojayeon <jojayeon6152@gmail.com>
2024-06-25 14:36:34 +0900

    :bulb: 조자연 : 제품 촉액 계산 함수 설명, 주석추가

--- src/client/ts/modules/selectProduct/allPriceProduct.ts
+++ src/client/ts/modules/selectProduct/allPriceProduct.ts
@@ -1,4 +1,11 @@
-// * 제품 총액 계산 함수
+/** 
+ * @jojayeon 24.06.25
+ * *제품을 총액 계산 
+ * @param selectedProducts -객체 메뉴의 
+ * @returns 
+ */
+
+// 객체 들어오면 product로 받아서 그 객체의 price의 가격만 덧셈하는 방식
 export function calculateTotalPrice(selectedProducts: any[]): number {
   // 선택한 제품들을 기반으로 총액 계산 로직 구현
   let totalPrice = 0;
