ff3d1fb10d1115c45a096f14288caa2c6f43d200
jojayeon <jojayeon6152@gmail.com>
2024-06-25 17:29:46 +0900

    :package: 조자연: allpriceproduct.ts 함수이름 default 수정

--- src/client/ts/modules/selectProduct/allPriceProduct.ts
+++ src/client/ts/modules/selectProduct/allPriceProduct.ts
@@ -2,15 +2,15 @@
  * @jojayeon 24.06.25
  * *제품을 총액 계산 
  * @param selectedProducts -객체 변수 넣어주면됨  
- * @returns 
+ * @returns - totalPrice, 총액 출력
  */
 
 // 객체 들어오면 product로 받아서 그 객체의 price의 가격만 덧셈하는 방식
-export function calculateTotalPrice(selectedProducts: any[]): number {
+export default((selectedProducts: { name: string, price: number }[]) => {
   // 선택한 제품들을 기반으로 총액 계산 로직 구현
   let totalPrice = 0;
   selectedProducts.forEach(product => {
     totalPrice += product.price;
   });
   return totalPrice;
-}
+})
