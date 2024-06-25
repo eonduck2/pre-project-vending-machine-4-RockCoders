5a6a490e6bc8b3dd157b374ca86ddc7748edc686
jojayeon <jojayeon6152@gmail.com>
2024-06-25 17:30:27 +0900

    :package: 조자연: selectProductOutput.ts 함수이름 default 수정

--- src/client/ts/modules/selectProduct/selectProductOutput.ts
+++ src/client/ts/modules/selectProduct/selectProductOutput.ts
@@ -1,7 +1,11 @@
-// * 선택한 제품 출력하는 함수
+/** 
+ * @jojayeon 24.06.25
+ * * 선택한 제품 출력하는 함수
+ * @param selectedProducts - 메뉴에 들어가 있는 객체 
+ * @returns - selectedProducts 원래가져오 객체 리턴
+ */
 
-
-export function displaySelectedProducts(selectedProducts: { name: string, price: number }[]) {
+export default ((selectedProducts: { name: string, price: number }[]) => {
   const productListElement = document.getElementById('total-price');
   if (productListElement) {
     productListElement.innerHTML = ''; // 기존 목록 삭제
@@ -10,5 +14,7 @@ export function displaySelectedProducts(selectedProducts: { name: string, price:
       li.textContent = `${product.name} : ${product.price}원`;
       productListElement.appendChild(li);
     });
+
   }
-}
+  return selectedProducts
+})
