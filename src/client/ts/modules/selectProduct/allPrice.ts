2c9dd91c7a7b568fdf69fca8e4fa649a37a529f7
jojayeon <jojayeon6152@gmail.com>
2024-06-25 14:40:21 +0900

    :sparkles: 조자연 : 총액 출력하는 함수 생성

--- src/client/ts/modules/selectProduct/allPrice.ts
+++ src/client/ts/modules/selectProduct/allPrice.ts
@@ -1,1 +1,9 @@
 // * 총액 출력하는 함수
+
+export function displayTotalPrice(totalPrice: number) {
+  const totalPriceElement = document.getElementById('total-price');
+  if (totalPriceElement) {
+    //이 부분이 표시되는 부분이다. 실질적으로 보이는 구역 
+    totalPriceElement.innerText = `총액: ${totalPrice}원`;
+  }
+}
