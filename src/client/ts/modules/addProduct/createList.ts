4a08d2e949c38dd4682818d1d08e7f4a50de1e7c
moonhr <moonhr153@gmail.com>
2024-06-25 09:42:24 +0900

    :memo:문혜림:list 코드

--- src/client/ts/modules/addProduct/createList.ts
+++ src/client/ts/modules/addProduct/createList.ts
@@ -1,1 +1,10 @@
 // * 제품정보가 업데이트되면 리스트를 추가해주는 함수
+const sqlite3 = require('sqlite3');
+
+const createDatabase : Function = ():void=>{
+  const db = new sqlite3.Database('./test.db');
+
+  // const createQuery = db.prepare("CREATE TABLE team4 (id TEXT PRIMARY KEY, )");
+
+
+}
