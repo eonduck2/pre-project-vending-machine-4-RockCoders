ab579771967081dd517cfc78aba3562572f3aafd
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-25 16:03:56 +0900

    :twisted_rightwards_arrows:유호영: server ts화

---
+++ src/server/server.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+import path from "path";
+
+//* ESM 방식의 __dirname 재정의
+const __dirname = path.resolve();
+
+const app = express();
+
+//* 미들웨어로 등록하기 위한 경로 설정
+const publicPath = path.join(__dirname, "public");
+const srcPath = path.join(__dirname, "src");
+const distPath = path.join(__dirname, "dist");
+
+//* 환경 변수로 지정된 포트가 없으면 8080을 사용합니다.
+const PORT = process.env.PORT ?? 8080;
+
+//* 미들웨어 등록
+app.use(express.static(publicPath));
+app.use('/dist', express.static(distPath));
+app.use(express.static(srcPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  return res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.post("/create", (req: Request, res: Response) => {
+  const body = req.body;
+  console.log(body); //* 요청된 폼 데이터가 제대로 출력됩니다.
+  return res.redirect("/");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
