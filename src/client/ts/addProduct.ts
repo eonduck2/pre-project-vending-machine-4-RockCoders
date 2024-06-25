da559092a298475e9e36088f27b6d312112fa4b1
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-25 12:13:17 +0900

    :sparkles:유호영:제품 추가버튼 누를 시 form 생성, submit시 form 숨김

--- src/client/ts/addProduct.ts
+++ src/client/ts/addProduct.ts
@@ -1,1 +1,5 @@
 // * 제품 추가
+
+import { create } from "./modules/addProduct/addProductEvent.js";
+
+create();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    './public/*.html',
-    './src/**/*.js',
+    './public/**/*.html',
+    './dist/**/*.js',
   ],
   theme: {
     extend: {},
