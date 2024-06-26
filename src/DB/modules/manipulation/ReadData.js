d3996632e97ce85fc2d2b73db2eee6dee0e8bd9b
eonduck2 <whdtnxd@gmail.com>
2024-06-26 23:41:32 +0900

    :recycle: 이종수: 데이터 리딩 기능 리팩토링 완료

---
+++ src/DB/modules/manipulation/ReadData.js
@@ -0,0 +1,65 @@
+import DBConnector from "../../DBConnector.js";
+
+class AbstractReadData {
+  constructor(fileWithPath) {
+    if (new.target === AbstractReadData) {
+      throw new Error("AbstractReadData 클래스는 직접 인스턴스화 할 수 없음");
+    }
+    super(fileWithPath);
+  }
+
+  readRecord(tableName, column, value, log = false) {}
+
+  readRecordsAll(tableName, log = false) {}
+}
+
+export default class ReadData extends AbstractReadData {
+  /**
+   * @eonduck2 24.06.21
+   * * 테이블과 컬럼명, 해당 컬럼 내의 값으로 데이터를 조회
+   * @param { string } tableName 조회할 테이블
+   * @param { string | number } column 조회할 테이블의 컬럼
+   * @param { string | number } value 조회할 테이블의 컬럼의 값
+   * @param { boolean } log true 값으로 보낼 시, 데이터 리턴과 동시에 console에 logging
+   * @returns { promise } 특정 컬럼의 데이터가 포함된 Promise
+   */
+  readRecord(tableName, column, value, log = false) {
+    const sql = `SELECT * FROM ${tableName} WHERE ${column} = ?`;
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, [value], (err, rows) => {
+        if (err) {
+          throw new Error(`쿼리문 조회 에러`);
+        } else if (log) {
+          console.log(rows);
+          resolve(rows);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  /**
+   * @eonduck2 24.06.21
+   * * 테이블 이름으로 해당 테이블 내의 모든 데이터 조회
+   * @param { string } tableName 전체 데이터를 조회할 테이블
+   * @param { boolean } log true 값으로 보낼 시, 데이터 리턴과 동시에 console에 logging
+   * @returns { promise } 특정 테이블의 전체 데이터가 포함된 Promise
+   */
+  readRecordsAll(tableName, log = false) {
+    const sql = `SELECT * FROM ${tableName}`;
+
+    return new Promise((resolve, reject) => {
+      this.db.all(sql, (err, rows) => {
+        if (err) {
+          throw new Error(`"${tableName}" 테이블 조회 실패`);
+        } else if (log) {
+          console.log(rows);
+          resolve(rows);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+}
