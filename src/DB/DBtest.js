1f1f7e791713778600c324e5a22d8faeed2d62b0
eonduck2 <whdtnxd@gmail.com>
2024-06-26 23:55:35 +0900

    :recycle: 이종수: 컬럼 추가 기능 리팩토링 완료

--- src/DB/DBtest.js
+++ src/DB/DBtest.js
@@ -2,9 +2,13 @@ import DBConnector from "./DBConnector.js";
 import TableCreator from "./modules/table/TableCreator.js";
 import ReadData from "./modules/manipulation/ReadData.js";
 import createData from "./modules/manipulation/CreateData.js";
+import AddColumn from "./modules/table/column/AddColumn.js";
 
 const reader = new ReadData(`test.db`);
 const creator = new createData(`test.db`);
+const adder = new AddColumn(`test.db`);
 
-creator.createRecord(`test_tble`, { name: `테스트1` });
-reader.readRecordsAll(`test_tble`, true);
+// creator.createRecord(`test_tble`, { name: `테스트1` });
+// reader.readRecordsAll(`test_tble`, true);
+
+adder.addColumn(`test_tble`, `age`, "TEXT");
