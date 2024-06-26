ef509f045846713e9beac35e97ba135a39250124
moonhr <moonhr153@gmail.com>
2024-06-26 18:03:04 +0900

    :sparkles:문혜림:입금버튼 클릭 발생 시 로컬에 값 더해서 저장과 출력

--- src/client/ts/modules/amount/priceInput.ts
+++ src/client/ts/modules/amount/priceInput.ts
@@ -1,1 +1,29 @@
 // * 입금하기 버튼 누르면 실행 될 함수 === 제품정보 출력하는 파일 
+import { validateAmount } from "./validateAmount";
+import { displayBalance } from "./displayBalance";
+import LocalStorageModel from '../../../../localStorage/localStorage'
+
+export const moneyButton = document.getElementById('money-button') as HTMLInputElement;
+
+moneyButton.addEventListener('click', ()=>{
+  
+  const moneyInput = document.getElementById('money-input') as HTMLInputElement;
+
+  //문자열로 들어온 값을 숫자로 반환함.
+  const money = parseInt(moneyInput.value, 10);
+
+  //input으로 들어온 값 검사하여 참일 때 실행
+  if(validateAmount(money)){
+    const storageManager = new LocalStorageModel();
+    //로컬스토리지.balance값 가져오기
+    let currentBalance: number | null = storageManager.getItem("balance")!;
+    //값 추가
+    currentBalance += money;
+    //값 저장
+    storageManager.setItem('balance', currentBalance);
+    //값 출력
+    displayBalance();
+  } else {
+    alert('입력된 값이 옳지 않습니다. 1000원 이상 10000원 이하만 입금 가능합니다.')
+  }
+})
