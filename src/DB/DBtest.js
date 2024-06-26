f88a1d38bb2c638d80a1aeb1ca83f768543e47e7
eonduck2 <whdtnxd@gmail.com>
2024-06-26 17:58:26 +0900

    :wrench: 이종수: 제약 조건 기능 관련 통합 위한 새로운 파일 생성

--- src/DB/DBtest.js
+++ src/DB/DBtest.js
@@ -1,4 +1,4 @@
 import DBConnector from "./DBConnector.js";
 import TableCreator from "./modules/table/TableCreator.js";
 
-const test = new TableCreator(`test.db`);
+// const test = new TableCreator(`test.db`);
