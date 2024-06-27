cbba7631a678f953647cc1772faa0cd618bfb4ae
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-27 14:31:34 +0900

    :fire:유호영:제품 추가 clickEvent 삭제

--- src/client/ts/modules/addProduct/hideCreateForm.ts
+++ src/client/ts/modules/addProduct/hideCreateForm.ts
@@ -1,3 +1,8 @@
+/**
+ * 2024.06.27 @crystal23733
+ * *createFormContainer div를 숨기는 함수
+ */
+
 export default () => {
   const createFormContainer = document.getElementById('create-form-container');
   if (createFormContainer) {
--- src/client/ts/modules/addProduct/showCreateForm.ts
+++ src/client/ts/modules/addProduct/showCreateForm.ts
@@ -1,3 +1,8 @@
+/**
+ * @crystal23733 2024.06.27
+ * * createFormContainer div를 나타내주는 함수
+ */
+
 export default () => {
   const createFormContainer = document.getElementById('create-form-container');
   if (createFormContainer) {
