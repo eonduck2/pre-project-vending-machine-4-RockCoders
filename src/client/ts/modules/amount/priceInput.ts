9490d04a6501885b4f3ac8197ff502a0c45cfb5b
moonhr <moonhr153@gmail.com>
2024-06-27 20:27:40 +0900

    :package:문혜림:fetch 응답시 반응 생성, 응답값 타입 지정

--- src/client/ts/modules/amount/priceInput.ts
+++ src/client/ts/modules/amount/priceInput.ts
@@ -15,6 +15,8 @@ export async function priceInput() {
   //문자열로 들어온 값을 숫자로 반환함.
   const money = parseInt(moneyInput.value, 10);
 
+  console.log('일단 함수 실행중');
+
   //input으로 들어온 값 검사하여 참일 때 실행
   if (validateAmount(money)) {
     const storageManager = new LocalStorageModel();
@@ -27,31 +29,38 @@ export async function priceInput() {
     //값 출력
     displayBalance();
 
-    // 서버로부터 제품 정보 가져오기
-    const response = await fetch('/products');
-    const data = await response.json();
-    const products = data.products;
-    const menuContent = document.getElementById('menu-content') as HTMLDivElement;
-    menuContent.innerHTML = '';
+    try {
+      // 서버로부터 제품 정보 가져오기
+      const response = await fetch('/products');  // 서버의 /products 경로로 GET 요청
+      const data = await response.json();
+      const products = data.products;
+      const menuContent = document.getElementById('menu-content') as HTMLDivElement;
+      menuContent.innerHTML = '';
 
-    // 각 제품을 div 요소로 추가
-    products.forEach(product => {
-      if (product.price <= currentBalance) {
-
-        const productName: string = product.name;
-        const productPrice: number = product.price;
-
-        const productDiv = document.createElement('div');
-        productDiv.textContent = `<div class="w-full h-44 flex flex-col items-center bg-gray-300 relative"><div class="text-base w-4/5 overflow-auto absolute top-1/4">${productName}</div><div class="w-4/5 h-7 rounded-full bg-white absolute top-2/3 flex justify-center items-center">${productPrice}</div>`
-        menuContent.appendChild(productDiv)
-      } else {
-
+      type productType = {
+        id : number,
+        name : string,
+        price : number
       }
-    });
+      
+      // 각 제품을 div 요소로 추가
+      products.forEach((product : productType) => {
+        if (product.price <= currentBalance) {
 
+          //todo id 태그에 넣어주기!!!
+          const productId: number = product.id;
+          const productName: string = product.name;
+          const productPrice: number = product.price;
+
+          const productDiv = document.createElement('div');
+          productDiv.textContent = `<div class="w-full h-44 flex flex-col items-center bg-gray-300 relative"><div class="text-base w-4/5 overflow-auto absolute top-1/4">${productName}</div><div class="w-4/5 h-7 rounded-full bg-white absolute top-2/3 flex justify-center items-center">${productPrice}</div>`
+
+          menuContent.appendChild(productDiv)
+        }
+      });
+    }
+    catch (error) { console.error('제품 정보를 가져오는 중 오류 발생:', error); }
   } else {
     alert('입력된 값이 옳지 않습니다. 1000원 이상 10000원 이하만 입금 가능합니다.');
   }
 };
-
-document.getElementById('money-button')?.addEventListener('click', priceInput);
