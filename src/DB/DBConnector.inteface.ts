a0f00fd4048d1665f81c65a23415e760324666e3
eonduck2 <whdtnxd@gmail.com>
2024-06-27 13:38:08 +0900

    :recycle: 이종수: DBConnector.ts 리팩토링 #1

--- src/DB/DBConnector.inteface.ts
+++ src/DB/DBConnector.inteface.ts
@@ -1,9 +1,9 @@
 import IDB from "./interfaces/Db.interface";
 import IFileWithPath from "./interfaces/FileWithPath.interface";
-import IClose from "./interfaces/Close.inteface";
+import IDBCloser from "./interfaces/Close.inteface";
 
 export default interface IDBConnector {
   db: IDB;
   fileWithPath: IFileWithPath;
-  close(): IClose;
+  close(): IDBCloser;
 }
--- src/DB/DBConnector.ts
+++ src/DB/DBConnector.ts
@@ -1,27 +1,25 @@
 import sqlite3, { Database } from "sqlite3";
+import IDBConnector from "./DBConnector.inteface";
+import IDB from "./interfaces/Db.interface";
+import IFileWithPath from "./interfaces/FileWithPath.interface";
+import IDBCloser from "./interfaces/Close.inteface";
 
 const sqlite3VM = sqlite3.verbose();
 
-interface IDBConnector {
-  db: Database;
-  fileWithPath: string;
-  close(): void;
-}
-
 export default class DBConnector implements IDBConnector {
-  protected db;
-  private fileWithPath;
+  db: IDB;
+  fileWithPath: IFileWithPath;
   /**
    * @eonduck2 24.06.21
    * * 인자로 받은 경로에 존재하는 DB 파일을 연결(존재하지 않을 시 생성)
    * @param { String } fileWithPath 문자열 타입의 경로(eg: src/test.db)
    */
-  constructor(fileWithPath: string) {
+  constructor(fileWithPath: IFileWithPath) {
     if (new.target === DBConnector) {
       throw new Error("DBManager 클래스는 직접 인스턴스화 할 수 없음");
     }
     this.fileWithPath = fileWithPath;
-    this.db = new sqlite3VM.Database(fileWithPath, (err) => {
+    this.db = new sqlite3VM.Database(fileWithPath, (err: Error) => {
       if (err) {
         throw new Error("DB 연결 실패");
       } else {
@@ -33,8 +31,8 @@ export default class DBConnector implements IDBConnector {
    * @eonduck2 24.06.22
    * * DB와의 연결 해제
    */
-  protected close(): void {
-    this.db.close((err) => {
+  close(): IDBCloser {
+    this.db.close((err: Error) => {
       if (err) {
         throw new Error("DB 커넥션 close 오류");
       } else {
