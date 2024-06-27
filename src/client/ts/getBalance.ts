c581f6bb1178dc47025d87c28a0e25684101cd46
yuxincxoi <yjyj0774@gmail.com>
2024-06-27 23:16:39 +0900

    :wrench: 최유진 : 잔액 계산 함수 생성

--- src/client/ts/getBalance.ts
+++ src/client/ts/getBalance.ts
@@ -1,23 +1,5 @@
-import LocalStorageModel from '../../localStorage/localStorage';
-
 /**
- * @yuxincxoi 24.06.24
+ * @yuxincxoi 24.06.27
  * * 구매하기 버튼 클릭 시 호출될 함수
- * * 로컬스토리지에서 잔액 가져와서 출력
+ * * 잔액 계산하는 함수
  */
-
-// * 로컬스토리지 객체 생성
-const localData = new LocalStorageModel();
-
-const getBalance = () => {
-  try {
-    // * 로컬스토리지에서 잔액 가져오기
-    let balance = localData.getItem('balance');
-  } catch (error) {
-    console.error('로컬스토리지에 데이터를 저장하는 중 오류가 발생했습니다:', error);
-  }
-}
-
-// todo : balance 출력
-
-export default getBalance();
