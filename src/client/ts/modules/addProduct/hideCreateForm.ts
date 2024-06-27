fef6fa0d5ff7a6f9ea8515c7a244098ea4029a08
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-27 13:10:41 +0900

    :sparkles:유호영:create모듈 추가

---
+++ src/client/ts/modules/addProduct/hideCreateForm.ts
@@ -0,0 +1,6 @@
+export default () => {
+  const createFormContainer = document.getElementById('create-form-container');
+  if (createFormContainer) {
+      createFormContainer.classList.add('hidden');
+  }
+};
---
+++ src/client/ts/modules/addProduct/showCreateForm.ts
@@ -0,0 +1,6 @@
+export default () => {
+  const createFormContainer = document.getElementById('create-form-container');
+  if (createFormContainer) {
+      createFormContainer.classList.remove('hidden');
+  }
+};
