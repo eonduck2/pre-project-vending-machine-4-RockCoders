519abedc6f7880275511fb5db23aab525119abf7
moonhr <moonhr153@gmail.com>
2024-06-28 10:00:33 +0900

    :package:문혜림:제작된 div에 id 추가

--- src/client/ts/modules/amount/priceInput.ts
+++ src/client/ts/modules/amount/priceInput.ts
@@ -31,6 +31,10 @@ export async function priceInput() {
       // 서버로부터 제품 정보 가져오기
       const response = await fetch('/products');  // 서버의 /products 경로로 GET 요청
       const data = await response.json();
+
+      if (!data.products) {
+        throw new Error('제품 정보가 응답 데이터에 없습니다.');
+      }
       const products = data.products;
       const menuContent = document.getElementById('menu-content') as HTMLDivElement;
       menuContent.innerHTML = '';
@@ -51,7 +55,9 @@ export async function priceInput() {
           const productPrice: number = product.price;
 
           const productDiv = document.createElement('div');
-          productDiv.textContent = `<div class="w-full h-44 flex flex-col items-center bg-gray-300 relative"><div class="text-base w-4/5 overflow-auto absolute top-1/4">${productName}</div><div class="w-4/5 h-7 rounded-full bg-white absolute top-2/3 flex justify-center items-center">${productPrice}</div>`
+          productDiv.textContent = `<div class="w-full h-44 flex flex-col items-center bg-gray-300 relative">
+          <div class="text-base w-4/5 overflow-auto absolute top-1/4">${productId}</div>
+          <div class="text-base w-4/5 overflow-auto absolute top-2/4">${productName}</div><div class="w-4/5 h-7 rounded-full bg-white absolute top-2/3 flex justify-center items-center">${productPrice}</div>`
 
           menuContent.appendChild(productDiv)
         }
