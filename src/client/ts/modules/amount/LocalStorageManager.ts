a511f9060a7ec6ac8e5bcc1e35bf68ffcd491596
moonhr <moonhr153@gmail.com>
2024-06-28 16:46:42 +0900

    :memo:문혜림:getItem 주석 추가

--- src/client/ts/modules/amount/LocalStorageManager.ts
+++ src/client/ts/modules/amount/LocalStorageManager.ts
@@ -1,4 +1,10 @@
 export class LocalStorageManager {
+  /**
+   * @moonhr 24.06.28
+   * * 로컬스토리지에서 값을 가져와 있으면 숫자로 파싱해 반환, 없으면 0 반환
+   * @param key 로컬스토리지에서 가져올 항목의 키
+   * @returns 
+   */
   getItem(key: string): number {
     const item = localStorage.getItem(key);
     return item ? parseInt(item, 10) : 0;
