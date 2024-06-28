808e3f4e69b355660d4bfaaf3fc12574b1891c9e
yuxincxoi <yjyj0774@gmail.com>
2024-06-28 14:28:35 +0900

    :card_file_box: 최유진 : 구매하기버튼 클릭시 데이터베이스 history 테이블에 데이터 추가하는 기능 서버에서 구현

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import path from "path";
 import __dirname from "../modules/__dirname";
+import { BaseDataBaseManager } from "../DB/modules/DBMANAGER";
 
 const app = express();
 
@@ -11,6 +12,8 @@ const distPath = path.join(__dirname, "dist");
 
 //* 환경 변수로 지정된 포트가 없으면 8080을 사용합니다.
 const PORT = process.env.PORT ?? 8080;
+
+const dbManager = new BaseDataBaseManager('../../productList.db');
 
 //* 미들웨어 등록
 app.use(express.static(publicPath));
@@ -26,6 +29,35 @@ app.post("/create", (req: Request, res: Response) => {
   const body = req.body;
   console.log(body); //* 요청된 폼 데이터가 제대로 출력됩니다.
   return res.redirect("/");
+});
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+app.post('/purchase', (req, res) => {
+  const products : Product[] = req.body.products;
+
+  if (!Array.isArray(products)) {
+    return res.status(400).send('Invalid products list');
+  }
+
+  dbManager.createTable("history", {
+    "name" : "TEXT",
+    "price" : "INTEGER"
+  });
+
+  products.forEach(product => {
+    const record: Record<string, string | number> = {
+      name: product.name,
+      price: product.price
+    };
+    dbManager.createRecord("history", record);
+  });
+
+  dbManager.closeConnection()
+  res.send('Products added to history');
 });
 
 app.listen(PORT, () => {
