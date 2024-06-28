5765d3b3409e2cd8ed43b07704aa4204bd14f14f
eonduck2 <whdtnxd@gmail.com>
2024-06-28 16:47:49 +0900

    :package: 이종수: 테이블 내 데이터 삭제 기능 인터페이스 정의

--- src/DB/modules/manipulation/DeleteData.interface.ts
+++ src/DB/modules/manipulation/DeleteData.interface.ts
@@ -1,1 +1,4 @@
-deleteRecordsAll(tableName);
+import IDeleteRecord from "../../interfaces/manipulation/DeleteRecord.interface";
+import IDeleteRecordsAll from "../../interfaces/manipulation/DeleteRecordsAll.interface";
+
+export default interface IDeleteData extends IDeleteRecord, IDeleteRecordsAll {}
