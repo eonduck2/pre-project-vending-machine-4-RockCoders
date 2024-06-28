8e56001703ffd2cb780ccea3d4f8f0244bcd3f8a
eonduck2 <whdtnxd@gmail.com>
2024-06-28 16:43:02 +0900

    :card_file_box: 이종수: 테이블 업데이트 기능 관련 검증 완료

--- test.ts
+++ test.ts
@@ -1,10 +1,12 @@
 import tableCreator from "./src/DB/modules/table/TableCreator";
 import recordCreator from "./src/DB/modules/manipulation/CreateData";
 import recordReader from "./src/DB/modules/manipulation/ReadData";
+import recordUpdater from "./src/DB/modules/manipulation/UpdateData";
 
 // const creator = new recordCreator(`123.db`);
 // const testTable = new tableCreator(`123.db`);
 // const reader = new recordReader(`123.db`);
+const updater = new recordUpdater(`123.db`);
 
 const testColumn = {
   name: `TEXT`,
@@ -15,6 +17,13 @@ const testData = {
   name: `이종수`,
   age: 99,
 };
+
+const updateData = {
+  name: `만수무강`,
+  age: 90000,
+};
+
+updater.updateRecord(`test_tbl2`, `name`, `이종수`, updateData);
 
 // creator.serialize(() => {
 //   //   testTable.createTable(`test_tbl2`, testColumn);
