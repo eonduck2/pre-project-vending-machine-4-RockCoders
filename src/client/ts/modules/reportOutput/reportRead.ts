01f66f928c7f41c5a2c599c720769876f037fd47
jojayeon <jojayeon6152@gmail.com>
2024-06-28 12:39:23 +0900

    :memo: 조자연: 설명추가

--- src/client/ts/modules/reportOutput/reportRead.ts
+++ src/client/ts/modules/reportOutput/reportRead.ts
@@ -1,6 +1,10 @@
+/** @jojayeon 24.06.28
+ * * 보고서 출력 - 아직 모듈로 쪼개지 않음
+ */
+
 import ReadData from "../../../../DB/modules/manipulation/ReadData.js";
 
-// ReadData 클래스를 사용하여 데이터베이스에서 데이터를 읽어오는 함수
+// * ReadData 클래스를 사용하여 데이터베이스에서 데이터를 읽어오는 함수
 const readSalesData = async (table: string): Promise<{ name: string; price: number }[]> => {
   const reader = new ReadData(table);// db위치
   try {
@@ -11,7 +15,7 @@ const readSalesData = async (table: string): Promise<{ name: string; price: numb
   }
 };
 
-// 총 매출 계산 함수
+// * 총 매출 계산 함수
 async function calculateTotalRevenue(table: string): Promise<number> {
   try {
     const salesData = await readSalesData(table);
@@ -22,7 +26,8 @@ async function calculateTotalRevenue(table: string): Promise<number> {
   }
 }
 
-// 가장 많이 팔린 제품 계산 함수
+
+// * 가장 많이 팔린 제품 계산 함수
 async function calculateMostSoldProduct(table: string): Promise<string> {
   try {
     const salesData = await readSalesData(table);
@@ -43,7 +48,7 @@ async function calculateMostSoldProduct(table: string): Promise<string> {
   }
 }
 
-// 판매 보고서 생성 및 출력 함수
+// *판매 보고서 생성 및 출력 함수
 async function generateSalesReport(table : string) {
   try {
     const totalRevenue = await calculateTotalRevenue(table);
@@ -60,4 +65,7 @@ async function generateSalesReport(table : string) {
 }
 
 // 보고서 출력
-generateSalesReport('test_tble');// 동적인 느낌이아니라 이상한데
+generateSalesReport('test_tble');//어떤 테이블인지 모르겠어서 매개변수로 넣음
+//! 할일 
+//! - 모듈로 쪼개기 
+//! - DB name,price만 있는게 맞는지 확인 필요
