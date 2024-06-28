8b2254cb91e1d1ee0d8d49cfa9e411e584c5584d
jojayeon <jojayeon6152@gmail.com>
2024-06-28 19:18:32 +0900

    :bulb: 조자연: 모듈화해서 안 사용하는 모여 있는 함수 주석 처리

--- src/client/ts/modules/reportOutput/reportRead.ts
+++ src/client/ts/modules/reportOutput/reportRead.ts
@@ -1,188 +1,188 @@
-/** @jojayeon 24.06.28
- * * 보고서 출력하는 함수 제작 
- * * 보고서 출력 - 아직 모듈로 쪼개지 않음
- * 
- */
+// /** @jojayeon 24.06.28
+//  * * 보고서 출력하는 함수 제작 
+//  * * 보고서 출력 - 아직 모듈로 쪼개지 않음
+//  * 
+//  */
 
-//! 주소 맞는지 확인 필요
-//! 제일 아래 테이블도 확인 필요
+// //! 주소 맞는지 확인 필요
+// //! 제일 아래 테이블도 확인 필요
 
-import ReadData from "../../../../DB/modules/manipulation/ReadData.js";
+// import ReadData from "../../../../DB/modules/manipulation/ReadData.js";
 
-// * ReadData 클래스를 사용하여 데이터베이스에서 데이터를 읽어오는 함수
-const readSalesData = async (table: string): Promise<{ name: string; price: number }[]> => {
-  const reader = new ReadData(table);// db위치
-  try {
-    const data = await reader.readRecordsAll(table);
-    // console.log("5",data) - 혹시 어떻게 들어오는지 확인
-    return data.map((row: any) => ({ name: row.name, price: row.price }));
-  } finally {
-    reader.close();
-  }
-};
-
-
-// * 총 매출 계산 함수
-async function calculateTotalRevenue(table: string): Promise<number> {
-  try {
-    const salesData = await readSalesData(table);
-    const totalRevenue = salesData.reduce((total, item) => total + item.price, 0);
-    return totalRevenue;
-  } catch (error) {
-    throw new Error(`총 매출 계산 중 오류 발생: ${error}`);
-  }
-}
+// // * ReadData 클래스를 사용하여 데이터베이스에서 데이터를 읽어오는 함수
+// const readSalesData = async (table: string): Promise<{ name: string; price: number }[]> => {
+//   const reader = new ReadData(table);// db위치
+//   try {
+//     const data = await reader.readRecordsAll(table);
+//     // console.log("5",data) - 혹시 어떻게 들어오는지 확인
+//     return data.map((row: any) => ({ name: row.name, price: row.price }));
+//   } finally {
+//     reader.close();
+//   }
+// };
 
 
-// * 가장 많이 팔린 제품 계산 함수
-async function calculateMostSoldProduct(table: string): Promise<string[]> {
-  try {
-    const salesData = await readSalesData(table);
-    const productCount: { [key: string]: number } = {};
-
-    // 각 제품별 판매 횟수 계산
-    salesData.forEach((item) => {
-      const { name } = item;
-      productCount[name] = (productCount[name] || 0) + 1;
-    });
-
-    // 판매 횟수에 따라 정렬
-    const productCountArray = Object.entries(productCount);
-    productCountArray.sort((a, b) => b[1] - a[1]);
-
-    // 가장 많이 팔린 제품들을 모두 담을 배열
-    const mostSoldProducts: string[] = [];
-    const maxCount = productCountArray[0][1]; // 제일 많이 팔린 제품의 판매 횟수
-
-    // 가장 많이 팔린 제품들을 찾아 배열에 추가
-    for (const [product, count] of productCountArray) {
-      if (count === maxCount) {
-        mostSoldProducts.push(product);
-      } else {
-        break; // 판매 횟수가 maxCount보다 작으면 루프 종료
-      }
-    }
-
-    return mostSoldProducts;
-  } catch (error) {
-    throw new Error(`가장 많이 팔린 제품 계산 중 오류 발생: ${error}`);
-  }
-}
+// // * 총 매출 계산 함수
+// async function calculateTotalRevenue(table: string): Promise<number> {
+//   try {
+//     const salesData = await readSalesData(table);
+//     const totalRevenue = salesData.reduce((total, item) => total + item.price, 0);
+//     return totalRevenue;
+//   } catch (error) {
+//     throw new Error(`총 매출 계산 중 오류 발생: ${error}`);
+//   }
+// }
 
 
-// *판매 보고서 생성 및 출력 함수
-async function generateSalesReport(table : string) {
-  try {
-    const totalRevenue = await calculateTotalRevenue(table);
-    const mostSoldProduct = await calculateMostSoldProduct(table);
+// // * 가장 많이 팔린 제품 계산 함수
+// async function calculateMostSoldProduct(table: string): Promise<string[]> {
+//   try {
+//     const salesData = await readSalesData(table);
+//     const productCount: { [key: string]: number } = {};
 
-    const reportContainer = document.getElementById('reportContainer') as HTMLDivElement;
-    reportContainer.innerHTML = `
-      <div>총 매출: ${totalRevenue}원</div>
-      <div>가장 많이 팔린 제품: ${mostSoldProduct}</div>
-    `;
-  } catch (error) {
-    console.error('판매 보고서 생성 중 오류 발생:', error);
-  }
-}
+//     // 각 제품별 판매 횟수 계산
+//     salesData.forEach((item) => {
+//       const { name } = item;
+//       productCount[name] = (productCount[name] || 0) + 1;
+//     });
 
-// 보고서 출력
-generateSalesReport('test.db');
+//     // 판매 횟수에 따라 정렬
+//     const productCountArray = Object.entries(productCount);
+//     productCountArray.sort((a, b) => b[1] - a[1]);
 
-//! 할일 
-//! - 모듈로 나누기
-//// - 중복 체크 후 출력 추가 
-//? - DB name,price만 있는게 맞는지 확인 필요
-//?   - id 있을거 같은데?
+//     // 가장 많이 팔린 제품들을 모두 담을 배열
+//     const mostSoldProducts: string[] = [];
+//     const maxCount = productCountArray[0][1]; // 제일 많이 팔린 제품의 판매 횟수
 
-export default generateSalesReport;
+//     // 가장 많이 팔린 제품들을 찾아 배열에 추가
+//     for (const [product, count] of productCountArray) {
+//       if (count === maxCount) {
+//         mostSoldProducts.push(product);
+//       } else {
+//         break; // 판매 횟수가 maxCount보다 작으면 루프 종료
+//       }
+//     }
+
+//     return mostSoldProducts;
+//   } catch (error) {
+//     throw new Error(`가장 많이 팔린 제품 계산 중 오류 발생: ${error}`);
+//   }
+// }
 
 
-//모듈화 해서 나누고 저장하기
-// ---------------------------------------------------------------------------------------------------------------------------
-//* ReadReportDB
-import ReadData from "../../../../DB/modules/manipulation/ReadData.js";
+// // *판매 보고서 생성 및 출력 함수
+// async function generateSalesReport(table : string) {
+//   try {
+//     const totalRevenue = await calculateTotalRevenue(table);
+//     const mostSoldProduct = await calculateMostSoldProduct(table);
 
-// * ReadData 클래스를 사용하여 데이터베이스에서 데이터를 읽어오는 함수
-const readSalesData = async (table: string): Promise<{ name: string; price: number }[]> => {
-  const reader = new ReadData(table);// db위치
-  try {
-    const data = await reader.readRecordsAll(table);
-    // console.log("5",data) - 혹시 어떻게 들어오는지 확인
-    return data.map((row: any) => ({ name: row.name, price: row.price }));
-  } finally {
-    reader.close();
-  }
-};
-export default readSalesData;
+//     const reportContainer = document.getElementById('reportContainer') as HTMLDivElement;
+//     reportContainer.innerHTML = `
+//       <div>총 매출: ${totalRevenue}원</div>
+//       <div>가장 많이 팔린 제품: ${mostSoldProduct}</div>
+//     `;
+//   } catch (error) {
+//     console.error('판매 보고서 생성 중 오류 발생:', error);
+//   }
+// }
 
-// ---------------------------------------------------------------------------------------------------------------------------
-// * CalculateTotalSales
-const calculateTotalRevenue = async (table: string): Promise<number> => {
-  try {
-    const salesData = await readSalesData(table);
-    const totalRevenue = salesData.reduce((total, item) => total + item.price, 0);
-    return totalRevenue;
-  } catch (error) {
-    throw new Error(`총 매출 계산 중 오류 발생: ${error}`);
-  }
-}
+// // 보고서 출력
+// generateSalesReport('test.db');
 
-export default calculateTotalRevenue;
-// ---------------------------------------------------------------------------------------------------------------------------
-// * MostSoldItems
-const calculateMostSoldProduct = async (table: string): Promise<string[]> => {
-  try {
-    const salesData = await readSalesData(table);
-    const productCount: { [key: string]: number } = {};
+// //! 할일 
+// //! - 모듈로 나누기
+// //// - 중복 체크 후 출력 추가 
+// //? - DB name,price만 있는게 맞는지 확인 필요
+// //?   - id 있을거 같은데?
 
-    // 각 제품별 판매 횟수 계산
-    salesData.forEach((item) => {
-      const { name } = item;
-      productCount[name] = (productCount[name] || 0) + 1;
-    });
+// export default generateSalesReport;
 
-    // 판매 횟수에 따라 정렬
-    const productCountArray = Object.entries(productCount);
-    productCountArray.sort((a, b) => b[1] - a[1]);
 
-    // 가장 많이 팔린 제품들을 모두 담을 배열
-    const mostSoldProducts: string[] = [];
-    const maxCount = productCountArray[0][1]; // 제일 많이 팔린 제품의 판매 횟수
+// //모듈화 해서 나누고 저장하기
+// // ---------------------------------------------------------------------------------------------------------------------------
+// //* ReadReportDB
+// import ReadData from "../../../../DB/modules/manipulation/ReadData.js";
 
-    // 가장 많이 팔린 제품들을 찾아 배열에 추가
-    for (const [product, count] of productCountArray) {
-      if (count === maxCount) {
-        mostSoldProducts.push(product);
-      } else {
-        break; // 판매 횟수가 maxCount보다 작으면 루프 종료
-      }
-    }
+// // * ReadData 클래스를 사용하여 데이터베이스에서 데이터를 읽어오는 함수
+// const readSalesData = async (table: string): Promise<{ name: string; price: number }[]> => {
+//   const reader = new ReadData(table);// db위치
+//   try {
+//     const data = await reader.readRecordsAll(table);
+//     // console.log("5",data) - 혹시 어떻게 들어오는지 확인
+//     return data.map((row: any) => ({ name: row.name, price: row.price }));
+//   } finally {
+//     reader.close();
+//   }
+// };
+// export default readSalesData;
 
-    return mostSoldProducts;
-  } catch (error) {
-    throw new Error(`가장 많이 팔린 제품 계산 중 오류 발생: ${error}`);
-  }
-}
-export default calculateMostSoldProduct;
+// // ---------------------------------------------------------------------------------------------------------------------------
+// // * CalculateTotalSales
+// const calculateTotalRevenue = async (table: string): Promise<number> => {
+//   try {
+//     const salesData = await readSalesData(table);
+//     const totalRevenue = salesData.reduce((total, item) => total + item.price, 0);
+//     return totalRevenue;
+//   } catch (error) {
+//     throw new Error(`총 매출 계산 중 오류 발생: ${error}`);
+//   }
+// }
 
-// ---------------------------------------------------------------------------------------------------------------------------
-// * ReportOutput
-const generateSalesReport = async (table : string) => {
-  try {
-    const totalRevenue = await calculateTotalRevenue(table);
-    const mostSoldProduct = await calculateMostSoldProduct(table);
+// export default calculateTotalRevenue;
+// // ---------------------------------------------------------------------------------------------------------------------------
+// // * MostSoldItems
+// const calculateMostSoldProduct = async (table: string): Promise<string[]> => {
+//   try {
+//     const salesData = await readSalesData(table);
+//     const productCount: { [key: string]: number } = {};
 
-    const reportContainer = document.getElementById('reportContainer') as HTMLDivElement;
-    reportContainer.innerHTML = `
-      <div>총 매출: ${totalRevenue}원</div>
-      <div>가장 많이 팔린 제품: ${mostSoldProduct}</div>
-    `;
-  } catch (error) {
-    console.error('판매 보고서 생성 중 오류 발생:', error);
-  }
-}
-export default generateSalesReport;
+//     // 각 제품별 판매 횟수 계산
+//     salesData.forEach((item) => {
+//       const { name } = item;
+//       productCount[name] = (productCount[name] || 0) + 1;
+//     });
+
+//     // 판매 횟수에 따라 정렬
+//     const productCountArray = Object.entries(productCount);
+//     productCountArray.sort((a, b) => b[1] - a[1]);
+
+//     // 가장 많이 팔린 제품들을 모두 담을 배열
+//     const mostSoldProducts: string[] = [];
+//     const maxCount = productCountArray[0][1]; // 제일 많이 팔린 제품의 판매 횟수
+
+//     // 가장 많이 팔린 제품들을 찾아 배열에 추가
+//     for (const [product, count] of productCountArray) {
+//       if (count === maxCount) {
+//         mostSoldProducts.push(product);
+//       } else {
+//         break; // 판매 횟수가 maxCount보다 작으면 루프 종료
+//       }
+//     }
+
+//     return mostSoldProducts;
+//   } catch (error) {
+//     throw new Error(`가장 많이 팔린 제품 계산 중 오류 발생: ${error}`);
+//   }
+// }
+// export default calculateMostSoldProduct;
+
+// // ---------------------------------------------------------------------------------------------------------------------------
+// // * ReportOutput
+// const generateSalesReport = async (table : string) => {
+//   try {
+//     const totalRevenue = await calculateTotalRevenue(table);
+//     const mostSoldProduct = await calculateMostSoldProduct(table);
+
+//     const reportContainer = document.getElementById('reportContainer') as HTMLDivElement;
+//     reportContainer.innerHTML = `
+//       <div>총 매출: ${totalRevenue}원</div>
+//       <div>가장 많이 팔린 제품: ${mostSoldProduct}</div>
+//     `;
+//   } catch (error) {
+//     console.error('판매 보고서 생성 중 오류 발생:', error);
+//   }
+// }
+// export default generateSalesReport;
 
 
 
