2bc6c86d3943c08d93c35233fb6bc3b48b6a021c
eonduck2 <whdtnxd@gmail.com>
2024-06-28 15:13:56 +0900

    :fire: 이종수: 테이블 생성 인터페이스 임포트 구문 삭제

--- src/DB/modules/table/TableCreator.interface.ts
+++ src/DB/modules/table/TableCreator.interface.ts
@@ -1,7 +1,3 @@
-import IDB from "../../interfaces/Db.interface";
-import IDBCloser from "../../interfaces/Close.interface";
-import IDBConnector from "../../DBConnector.interface";
-
 export default interface ITableCreator {
   createTable(tableName: string, columns: object): void;
 }
