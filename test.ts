f22526462bb12a74660d7354404144e3ec356970
eonduck2 <whdtnxd@gmail.com>
2024-06-28 15:37:33 +0900

    :card_file_box: 이종수: DB 행 삽입 기능 관련 리팩토링 (구현 && 검증) 완료

--- test.ts
+++ test.ts
@@ -1,4 +1,23 @@
 import tableCreator from "./src/DB/modules/table/TableCreator";
+import recordCreator from "./src/DB/modules/manipulation/CreateData";
+
+const creator = new recordCreator(`123.db`);
+const testTable = new tableCreator(`123.db`);
+
+const testColumn = {
+  name: `TEXT`,
+  age: `INTEGER`,
+};
+
+const testData = {
+  name: `이종수`,
+  age: 99,
+};
+
+creator.serialize(() => {
+  testTable.createTable(`test_tbl2`, testColumn);
+  creator.createRecord(`test_tbl2`, testData);
+});
 // const DBConnector = require(`./src/DB/DBConnector`);
 // console.log(new DBConnector(`123.db`));
 
@@ -13,7 +32,6 @@ import tableCreator from "./src/DB/modules/table/TableCreator";
 
 // console.log(test);
 
-const testTable = new tableCreator(`123.db`);
 // new tableCreator(`123.db`).createTable(`test_tbl`, { name: "TEXT" });
 
 // testTable.serialize(() => {
