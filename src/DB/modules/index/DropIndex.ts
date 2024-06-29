55c75b5e0c9193f16b35402fa862e69a768afe16
eonduck2 <whdtnxd@gmail.com>
2024-06-29 22:37:01 +0900

    :recycle: 이종수: 인덱스 삭제 기능 ts 리팩토링 완료

---
+++ src/DB/modules/index/DropIndex.ts
@@ -0,0 +1,38 @@
+import DBManager from "../../DBMANAGER";
+import instanceChecker from "../../throw/instanceChecker";
+import IDropIndex from "./DropIndex.interface";
+
+abstract class AbstractDropIndex extends DBManager implements IDropIndex {
+  constructor(fileWithPath: string) {
+    super(fileWithPath);
+  }
+  public abstract dropIndex(indexName: string): void;
+}
+
+class ImplementedDropIndex extends AbstractDropIndex {
+  constructor(fileWithPath: string) {
+    instanceChecker(new.target, ImplementedDropIndex);
+    super(fileWithPath);
+  }
+  /**
+   * @eonduck2 24.06.23
+   * * 특정 인덱스를 삭제
+   * @param { string } indexName 삭제할 인덱스의 이름
+   */
+  dropIndex(indexName: string) {
+    const sql = `DROP INDEX IF EXISTS ${indexName}`;
+    this.db.run(sql, (err: Error) => {
+      if (err) {
+        throw new Error(`인덱스 삭제 에러`);
+      } else {
+        console.log(`"${indexName}" 인덱스 삭제 완료`);
+      }
+    });
+  }
+}
+
+export default class DropIndex extends ImplementedDropIndex {
+  constructor(fileWithPath: string) {
+    super(fileWithPath);
+  }
+}
