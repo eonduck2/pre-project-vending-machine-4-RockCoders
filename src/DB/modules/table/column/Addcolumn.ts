95fe3cf3d3caf69f26d8cb03f3ae2dc562b5d8fe
eonduck2 <whdtnxd@gmail.com>
2024-06-29 20:05:46 +0900

    :recycle: 이종수: 컬럼 추가 기능 ts 리팩토링 완료

---
+++ src/DB/modules/table/column/Addcolumn.ts
@@ -0,0 +1,52 @@
+import DBConnector from "../../../DBMANAGER";
+import instanceChecker from "../../../throw/instanceChecker";
+import IAddColumn from "./AddColumn.interface";
+
+abstract class AbstractAddColumn extends DBConnector implements IAddColumn {
+  constructor(fileWithPath: string) {
+    super(fileWithPath);
+  }
+
+  abstract addColumn(
+    tableName: string,
+    columnName: string | number,
+    columnType: string
+  ): void;
+}
+
+class ImplementedAddColumn extends AbstractAddColumn {
+  constructor(fileWithPath: string) {
+    instanceChecker(new.target, ImplementedAddColumn);
+    super(fileWithPath);
+  }
+  /**
+   * @eonduck2 24.06.22
+   * * 특정 테이블에 특정 컬럼을 추가하는 기능
+   * @param { string } tableName 대상으로 지정되는 테이블
+   * @param { string | number } columnName 추가될 컬럼 이름
+   * @param { string } columnType 추가될 컬럼 타입 예 - (TEXT, INTEGER)
+   */
+  addColumn(
+    tableName: string,
+    columnName: string | number,
+    columnType: string
+  ) {
+    const sql = `ALTER TABLE ${tableName} ADD COLUMN ${columnName} ${columnType}`;
+
+    this.db.run(sql, (err: Error) => {
+      if (err) {
+        throw new Error(
+          `"${tableName}" 테이블에 "${columnName}" 컬럼 추가 에러`
+        );
+      } else {
+        console.log(`"${tableName}" 테이블에 "${columnName}" 컬럼 추가 성공`);
+      }
+    });
+  }
+}
+
+export default class AddColumn extends ImplementedAddColumn {
+  constructor(fileWithPath: string) {
+    super(fileWithPath);
+  }
+}
