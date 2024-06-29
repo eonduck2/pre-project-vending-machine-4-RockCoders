3004d14000253157deb27d74ad3e7b706e5c2b1f
eonduck2 <whdtnxd@gmail.com>
2024-06-29 19:39:08 +0900

    :package: 이종수: 컬럼 재배치 인터페이스 기본 정의 완료

--- src/DB/interfaces/table/column/Reordering.interface.ts
+++ src/DB/interfaces/table/column/Reordering.interface.ts
@@ -1,3 +1,3 @@
-export default interface ICreateTable {
-  createTable(tableName: string, columns: object): void;
+export default interface IReordering {
+  reorderColumns(tableName: string, columns: object): void;
 }
