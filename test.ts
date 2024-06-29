46d0f016ddd9ff2a0c95582da3ca433c9b1dcc7e
eonduck2 <whdtnxd@gmail.com>
2024-06-29 20:19:46 +0900

    :card_file_box: 이종수: 컬럼 추가, 삭제, 재배치 기능 검증 완료

--- test.ts
+++ test.ts
@@ -1,5 +1,18 @@
+import creator from "./src/DB/modules/manipulation/insert/CreateData";
 import getTable from "./src/DB/modules/table/GetTableInfo";
+import adder from "./src/DB/modules/table/column/Addcolumn";
+import dropper from "./src/DB/modules/table/column/DropColumn";
+import reorderer from "./src/DB/modules/table/column/ReorderColumns";
 
-const TableInfoGetter = new getTable(`123.db`);
+// const testAdder = new adder(`123.db`);
+// const testDropper = new dropper(`123.db`);
+const testReorderer = new reorderer(`123.db`);
+// const testCreator = new creator(`123.db`);
 
-TableInfoGetter.getTableInfo(`test_tbl2`, true);
+// testCreator.createRecord(`test_tbl2`, { name: `만수`, age: 9999 });
+
+testReorderer.reorderColumns(`test_tbl2`, { age: "INTEGER", name: "TEXT" });
+
+// const TableInfoGetter = new getTable(`123.db`);
+
+// TableInfoGetter.getTableInfo(`test_tbl2`, true);
