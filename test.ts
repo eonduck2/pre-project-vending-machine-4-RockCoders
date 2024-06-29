e303c7790bc746eeb622b2ff4f20ca816b80e856
eonduck2 <whdtnxd@gmail.com>
2024-06-29 22:58:46 +0900

    :package: 이종수: 인덱스 생성, 삭제, 조회 기능 검증 완료

--- test.ts
+++ test.ts
@@ -4,28 +4,38 @@ import getTable from "./src/DB/modules/table/GetTableInfo";
 import adder from "./src/DB/modules/table/column/Addcolumn";
 import dropper from "./src/DB/modules/table/column/DropColumn";
 import reorderer from "./src/DB/modules/table/column/ReorderColumns";
+import CreateIndex from "./src/DB/modules/index/CreateIndex";
+import DropIndex from "./src/DB/modules/index/DropIndex";
+import GetIndexes from "./src/DB/modules/index/GetIndexes";
 
-const tableCreator = new TableCreator(`123.db`);
-// const testAdder = new adder(`123.db`);
-// const testDropper = new dropper(`123.db`);
-const testReorderer = new reorderer(`123.db`);
-const testCreator = new creator(`123.db`);
-testReorderer.beginTransaction(() => {
-  testReorderer.serialize(() => {
-    // testReorderer.reorderColumns(`test_tbl2`, { name: "TEXT", age: "INTEGER" });
-    // testReorderer.reorderColumns(`test_tbl2`, { age: "INTEGER", name: "TEXT" });
-    // testReorderer.commitTransaction();
-    // testReorderer.rollbackTransaction();
-    testReorderer.reorderColumns(`test_tbl3`, { name: "TEXT", age: "INTEGER" });
-    testReorderer.rollbackTransaction();
-    // testReorderer.commitTransaction();
-    // testReorderer.reorderColumns(`test_tbl3`, { age: "INTEGER", name: "TEXT" });
-    // testReorderer.rollbackTransaction();
-    testReorderer.reorderColumns(`test_tbl2`, { name: "TEXT", age: "INTEGER" });
-    testReorderer.commitTransaction();
-  });
-  // testReorderer.rollbackTransaction();
-});
+const indexCreator = new CreateIndex(`123.db`);
+const indexDeleter = new DropIndex(`123.db`);
+const indexGetter = new GetIndexes(`123.db`);
+
+// indexCreator.createIndex(`test_name_index3`, `test_tbl2`, `name`);
+indexGetter.getAllIndexes(`test_tbl2`);
+// indexDeleter.dropIndex(`test_index`);
+// const tableCreator = new TableCreator(`123.db`);
+// // const testAdder = new adder(`123.db`);
+// // const testDropper = new dropper(`123.db`);
+// const testReorderer = new reorderer(`123.db`);
+// const testCreator = new creator(`123.db`);
+// testReorderer.beginTransaction(() => {
+//   testReorderer.serialize(() => {
+//     // testReorderer.reorderColumns(`test_tbl2`, { name: "TEXT", age: "INTEGER" });
+//     // testReorderer.reorderColumns(`test_tbl2`, { age: "INTEGER", name: "TEXT" });
+//     // testReorderer.commitTransaction();
+//     // testReorderer.rollbackTransaction();
+//     testReorderer.reorderColumns(`test_tbl3`, { name: "TEXT", age: "INTEGER" });
+//     testReorderer.rollbackTransaction();
+//     // testReorderer.commitTransaction();
+//     // testReorderer.reorderColumns(`test_tbl3`, { age: "INTEGER", name: "TEXT" });
+//     // testReorderer.rollbackTransaction();
+//     testReorderer.reorderColumns(`test_tbl2`, { name: "TEXT", age: "INTEGER" });
+//     testReorderer.commitTransaction();
+//   });
+//   // testReorderer.rollbackTransaction();
+// });
 
 // tableCreator.createTable(`test_tbl3`, { name: "TEXT", age: "INTEGER" });
 // testCreator.createRecord(`test_tbl3`, { name: `만수`, age: 9999 });
