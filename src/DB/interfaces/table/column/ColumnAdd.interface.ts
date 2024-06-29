b803e7e5e91f1f5ae18e564afb578a0505736e96
eonduck2 <whdtnxd@gmail.com>
2024-06-29 19:57:01 +0900

    :package: 이종수: 컬럼 추가 인터페이스 정의 완료

--- src/DB/interfaces/table/column/ColumnAdd.interface.ts
+++ src/DB/interfaces/table/column/ColumnAdd.interface.ts
@@ -1,3 +1,7 @@
 export default interface IReordering {
-  reorderColumns(tableName: string, columns: object): void;
+  addColumn(
+    tableName: string,
+    columnName: string | number,
+    columnType: string
+  ): void;
 }
