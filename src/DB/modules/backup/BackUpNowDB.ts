d9118027eda78019c6342826cf56854da3d00bf9
eonduck2 <whdtnxd@gmail.com>
2024-06-29 23:11:42 +0900

    :recycle: 이종수: DB 백업 기능 TS 리팩토링 완료

---
+++ src/DB/modules/backup/BackUpNowDB.ts
@@ -0,0 +1,38 @@
+import DBManager from "../../DBMANAGER";
+import instanceChecker from "../../throw/instanceChecker";
+import IBackUpNowDB from "./BackUpNowDB.interface";
+import fs from "node:fs";
+
+abstract class AbstractBackUpNowDB extends DBManager implements IBackUpNowDB {
+  constructor(fileWithPath: string) {
+    super(fileWithPath);
+  }
+  public abstract backupDB(backupFilePath: string): void;
+}
+
+class ImplementedBackUpNowDB extends AbstractBackUpNowDB {
+  constructor(fileWithPath: string) {
+    instanceChecker(new.target, ImplementedBackUpNowDB);
+    super(fileWithPath);
+  }
+  /**
+   * @eonduck2 24.06.22
+   * @param { string } backupFilePath 현재 데이터 베이스를 백업시킬 경로와 파일명
+   * * 예시 - ./src/backUpFiles/backup_mydb.db
+   */
+  public backupDB(backupFilePath: string) {
+    fs.copyFile(this.fileWithPath, backupFilePath, (err) => {
+      if (err) {
+        throw new Error(`DB 백업 실행 오류`);
+      } else {
+        console.log(`"${backupFilePath}" DB 백업 성공`);
+      }
+    });
+  }
+}
+
+export default class BackUpNowDB extends ImplementedBackUpNowDB {
+  constructor(fileWithPath: string) {
+    super(fileWithPath);
+  }
+}
