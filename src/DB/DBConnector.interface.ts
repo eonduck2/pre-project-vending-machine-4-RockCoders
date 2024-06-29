08a246d622fea3174df0205c28559af24c6d26ba
eonduck2 <whdtnxd@gmail.com>
2024-06-29 21:26:35 +0900

    :package: 이종수: 기존 트랜잭션 컨트롤러, 따로 사용하기에는 무리가 있다고 판단, DBManager에 병합

--- src/DB/DBConnector.interface.ts
+++ src/DB/DBConnector.interface.ts
@@ -2,9 +2,15 @@ import IFileWithPath from "./interfaces/managing/FileWithPath.interface";
 import IDBCloser from "./interfaces/managing/Close.interface";
 import ISerialize from "./interfaces/managing/Serialize.interface";
 import IParallelize from "./interfaces/managing/Parallelize.interface";
+import IBeginTransaction from "./interfaces/transaction/BeginTransaction.interface";
+import ICommitTransaction from "./interfaces/transaction/CommitTransaction.interface";
+import IRollbackTransaction from "./interfaces/transaction/RollbackTransaction.interface";
 
 export default interface IDBManager
   extends IFileWithPath,
     IDBCloser,
     ISerialize,
-    IParallelize {}
+    IParallelize,
+    IBeginTransaction,
+    ICommitTransaction,
+    IRollbackTransaction {}
--- src/DB/DBMANAGER.ts
+++ src/DB/DBMANAGER.ts
@@ -8,9 +8,18 @@ abstract class AbstractDBManager implements IDBManager {
   protected abstract db: typeof database;
 
   abstract fileWithPath: string;
+
   abstract close(): void;
+
   abstract serialize(callback: Function): void;
+
   abstract parallelize(callback: Function): void;
+
+  abstract beginTransaction(callback: Function): void;
+
+  abstract commit(): void;
+
+  abstract rollback(): void;
 }
 
 class ImplementedDBManager extends AbstractDBManager {
@@ -57,6 +66,55 @@ class ImplementedDBManager extends AbstractDBManager {
   public parallelize(callback: Function): void {
     this.db.parallelize(() => {
       callback();
+    });
+  }
+
+  /**
+   * @eonduck2 24.06.22
+   * @param { function } callback 트랜잭션 작업이 진행될 콜백 함수
+   * * 트랜잭션 시작 기능
+   * * 단위 별로 묶는 작업 필요성 못 느낄 시, 사용할 필요 X
+   */
+  public beginTransaction(callback: Function) {
+    this.db.run("BEGIN TRANSACTION", (err: Error) => {
+      if (err) {
+        throw new Error(`트랜잭션 스타팅 에러`);
+      } else {
+        console.log("트랜잭션 시작");
+        callback();
+      }
+    });
+  }
+
+  /**
+   * @eonduck2 24.06.22
+   * * 트랜잭션 커밋
+   * * beginTransction 메서드 내, 쿼리 작업을 commit 할 때 사용
+   * * 단위 별로 묶는 작업 필요성 못 느낄 시, 사용할 필요 X
+   */
+  public commit() {
+    this.db.run("COMMIT", (err: Error) => {
+      if (err) {
+        throw new Error(`트랜잭션 커밋 에러`);
+      } else {
+        console.log("트랜잭션 커밋 완료");
+      }
+    });
+  }
+
+  /**
+   * @eonduck2 24.06.22
+   * * 트랜잭션 롤백
+   * * beginTransction 메서드 내, 쿼리 작업을 rollback 할 때 사용
+   * * 단위 별로 묶는 작업 필요성 못 느낄 시, 사용할 필요 X
+   */
+  public rollback() {
+    this.db.run("ROLLBACK", (err: Error) => {
+      if (err) {
+        throw new Error(`트랜잭션 롤백 에러`);
+      } else {
+        console.log("트랜잭션 롤백 완료");
+      }
     });
   }
 }
