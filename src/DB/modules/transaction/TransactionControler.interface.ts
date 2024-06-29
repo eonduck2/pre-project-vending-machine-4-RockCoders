b954640c02bd4221942de7ccf05eb6ff134912fc
eonduck2 <whdtnxd@gmail.com>
2024-06-29 21:27:08 +0900

    :fire: 이종수: 트랜잭션 관련 파일 삭제

--- src/DB/modules/transaction/TransactionControler.interface.ts
+++
@@ -1,8 +0,0 @@
-import IBeginTransaction from "../../interfaces/transaction/BeginTransaction.interface";
-import ICommitTransaction from "../../interfaces/transaction/CommitTransaction.interface";
-import IRollbackTransaction from "../../interfaces/transaction/RollbackTransaction.interface";
-
-export default interface ITransactionController
-  extends IBeginTransaction,
-    ICommitTransaction,
-    IRollbackTransaction {}
