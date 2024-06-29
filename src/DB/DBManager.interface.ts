f2fec5c3090a5cab6aaba5fb3ba30a369c418451
eonduck2 <whdtnxd@gmail.com>
2024-06-30 01:02:01 +0900

    :pencil2: 이종수: 'DBManager.interface.ts' 임포트 구문 '.js' 추가

--- src/DB/DBManager.interface.ts
+++ src/DB/DBManager.interface.ts
@@ -1,11 +1,11 @@
-import IFileWithPath from "./interfaces/managing/FileWithPath.interface";
-import IDBCloser from "./interfaces/managing/Close.interface";
-import ISerialize from "./interfaces/managing/Serialize.interface";
-import IParallelize from "./interfaces/managing/Parallelize.interface";
-import IBeginTransaction from "./interfaces/transaction/BeginTransaction.interface";
-import ICommitTransaction from "./interfaces/transaction/CommitTransaction.interface";
-import IRollbackTransaction from "./interfaces/transaction/RollbackTransaction.interface";
-import IOptimize from "./interfaces/managing/Optimize.interface";
+import IFileWithPath from "./interfaces/managing/FileWithPath.interface.js";
+import IDBCloser from "./interfaces/managing/Close.interface.js";
+import ISerialize from "./interfaces/managing/Serialize.interface.js";
+import IParallelize from "./interfaces/managing/Parallelize.interface.js";
+import IBeginTransaction from "./interfaces/transaction/BeginTransaction.interface.js";
+import ICommitTransaction from "./interfaces/transaction/CommitTransaction.interface.js";
+import IRollbackTransaction from "./interfaces/transaction/RollbackTransaction.interface.js";
+import IOptimize from "./interfaces/managing/Optimize.interface.js";
 
 export default interface IDBManager
   extends IFileWithPath,
