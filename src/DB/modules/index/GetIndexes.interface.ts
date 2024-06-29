ec760c09fbaa693774547c585302e3d4938523b5
eonduck2 <whdtnxd@gmail.com>
2024-06-29 22:20:28 +0900

    :package: 이종수: 구현 폴더 내, 인덱스 조회 기능 인터페이스 정의 완료

---
+++ src/DB/modules/index/GetIndexes.interface.ts
@@ -0,0 +1,4 @@
+import IReadIndexes from "../../interfaces/index/ReadIndexes.interface";
+import IReadAllIndexes from "../../interfaces/index/ReadAllIndexes.interface";
+
+export default interface IGetIndexes extends IReadIndexes, IReadAllIndexes {}
