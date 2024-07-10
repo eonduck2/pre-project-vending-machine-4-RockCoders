e5e0348fc5d9fe27f0a6403d7a2729cf5ffda378
yuxincxoi <yjyj0774@gmail.com>
2024-07-10 18:20:15 +0900

    :recycle: 최유진 : 입금액 및 잔액 client에서 가져와서 계산하도록 수정

--- src/client/ts/modules/purchace/balance.ts
+++ src/client/ts/modules/purchace/balance.ts
@@ -4,26 +4,30 @@ import { UIManager } from "../amount/UIManager.js";
  * @yuxincxoi 24.06.27
  * * 구매하기 버튼 클릭 시 호출될 함수
  * * 잔액 계산해서 로컬스토리지에 저장하고 출력하는 함수
- * @param {number} amount 입금액
- * @param {number} totalPrice 구매금액
- * @returns {number}
  */
 
 
-export default (amount : number, totalPrice : number|null) => {
-  // * 선택한 제품이 없으면(구매금액이 존재하지 않으면) 실행
-  if(totalPrice === null) {
-    alert("선택한 제품이 없습니다. 제품을 선택해주세요.");
-  } else {
-    // * 잔액 계산 : 입금액 - 구매금액
-    const getBalance = amount - totalPrice;
+export default () => {
+  // * 입금액과 잔액 가져오기
+  const amountElement = document.getElementById('balance')?.innerHTML;
+  const totalPriceElement = document.getElementById('total-price')?.innerHTML;
+  if (amountElement && totalPriceElement) {
+    const amount = parseInt(amountElement);
+    const totalPrice = parseInt(totalPriceElement);
+    
+    // * 선택한 제품이 없으면(구매금액이 존재하지 않으면)
+    if(totalPrice === null) {
+      alert("선택한 제품이 없습니다. 제품을 선택해주세요.");
+    } else { // * 선택한 제품(구매금액)이 존재한다면
+      // * 잔액 계산 : 입금액 - 구매금액
+      const getBalance = amount - totalPrice;
+  
+      // * 로컬스토리지에 잔액 저장
+      localStorage.setItem("balance", String(getBalance))
 
-    // * 로컬스토리지에 잔액 저장
-    localStorage.setItem("balance", String(getBalance))
-
-
-    // * 잔액 출력 함수 호출
-    const balance = new UIManager();
-    balance.displayBalance();
+      // * 잔액 출력 함수 호출
+      const balance = new UIManager();
+      balance.displayBalance();
+    }
   }
 }
