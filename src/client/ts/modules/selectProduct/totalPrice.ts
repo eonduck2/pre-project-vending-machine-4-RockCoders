e82144a278794914b64c6467da4d3ab7030ff8c4
yuxincxoi <yjyj0774@gmail.com>
2024-07-10 17:16:18 +0900

    :bulb: 최유진 : 주석 추가

--- src/client/ts/modules/selectProduct/totalPrice.ts
+++ src/client/ts/modules/selectProduct/totalPrice.ts
@@ -8,13 +8,18 @@ let total = 0;
 
 export default (target: HTMLElement) => {
   try {
+    // * 선택된 제품의 가격을 숫자 형식으로 불러오기
     const productPrice = target.querySelector(".rounded-full")!.textContent;
     if(productPrice){
       const price = parseFloat(productPrice);
       
+      // price가 숫자 형식이면
       if (!isNaN(price)) {
+        // * total에 선택한 제품의 가격 누적 계산
         total += price;
+        // * 계산된 총액이 존재한다면
         if(total){
+          // * 총액 출력
           const totalPrice = document.getElementById("total-price");
           if (totalPrice) {
             totalPrice.innerHTML = total.toString();
