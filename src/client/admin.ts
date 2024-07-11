5e88fbf9cb2009160b27aed2ae018fc6c658444d
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-11 17:53:02 +0900

    :sparkles:유호영:관리자페이지 제품리스트 추가

--- src/client/admin.ts
+++ src/client/admin.ts
@@ -1,12 +1,27 @@
 import CreateFormManager from "./ts/modules/addProduct/createFormManager.js";
+import { UIManager } from "./ts/modules/adminList.js";
 import DeleteFormManager from "./ts/modules/deleteProduct/deleteFormManager.js";
 import ReportButtonManager from "./ts/modules/reportHandle/reportButtonManager.js";
 
 import UpdateFormManager from "./ts/modules/updateProduct/updateFormManager.js";
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
+  const uiManager = new UIManager();
+
+  // 서버에서 제품 목록을 가져오기
+  fetch('/products')
+    .then(response => response.json())
+    .then(products => {
+      uiManager.displayProducts(products);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   new CreateFormManager();
   new UpdateFormManager();
   new DeleteFormManager();
   new ReportButtonManager();
 });
+
