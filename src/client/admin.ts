f795e600e3abb2c286dc35aaafe5059a7215e3f9
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-13 00:14:27 +0900

    :sparkles:유호영__
    제품 수정 서버측으로 POST요청 후 존재하지 않을 경우 json으로 에러메세지를 가지고 와 HTML태그에 할당하는 함수를 추가함

--- src/client/admin.ts
+++ src/client/admin.ts
@@ -1,7 +1,9 @@
 import CreateFormManager from "./ts/modules/addProduct/createFormManager.js";
 import { UIManager } from "./ts/modules/adminList.js";
 import DeleteFormManager from "./ts/modules/deleteProduct/deleteFormManager.js";
+import handleDeleteError from "./ts/modules/deleteProduct/handleDeleteError.js";
 import ReportButtonManager from "./ts/modules/reportHandle/reportButtonManager.js";
+import handleUpdateError from "./ts/modules/updateProduct/handleUpdateError.js";
 
 import UpdateFormManager from "./ts/modules/updateProduct/updateFormManager.js";
 
@@ -23,5 +25,19 @@ document.addEventListener('DOMContentLoaded', () => {
   new UpdateFormManager();
   new DeleteFormManager();
   new ReportButtonManager();
+  const updateForm = document.getElementById('update-form') as HTMLFormElement;
+  const deleteForm = document.getElementById('delete-form') as HTMLFormElement;
+
+  /**
+   * @crystal23733 24.07.13
+   * * 제품 수정, 삭제 시 HTML태그 핸들링하는 함수
+   */
+  if (updateForm) {
+    updateForm.addEventListener('submit', handleUpdateError);
+  }
+
+  if (deleteForm) {
+    deleteForm.addEventListener('submit', handleDeleteError);
+  }
 });
 
